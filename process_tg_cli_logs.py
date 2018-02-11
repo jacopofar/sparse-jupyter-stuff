e759c8cc97ce9e56bca206ba4fc5d85875c2b0ba
jacopofar <jacopofar@users.noreply.github.com>
2018-02-11 21:26:50 +0100

    Skip errors

--- process_tg_cli_logs.py
+++ process_tg_cli_logs.py
@@ -14,35 +14,44 @@ import json
 import os
 
 # parameters that could me moved to CLI arguments if needed. But I'm lazy
-messages_file = open('messages.tsv', 'w', encoding='utf8')
-online_presence_file = open('online.tsv', 'w', encoding='utf8')
+messages_file = open('messages.tsv', 'w', encoding='utf8', newline='\n')
+online_presence_file = open('online.tsv', 'w', encoding='utf8', newline='\n')
 tg_cli_logs_folder = os.path.expanduser('~/Documents/my/tgdocker/')
 
 # No with statement here, if it fails just stops
 messages_file.write('user_id\tuser_print_name\tto_id\tto_print_name\ttimestamp\ttext\n')
 online_presence_file.write('user_id\tuser_print_name\ttimestamp\n')
 
-message_writer = csv.writer(messages_file, delimiter='\t')
-presence_writer = csv.writer(online_presence_file, delimiter='\t')
+message_writer = csv.writer(messages_file, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
+presence_writer = csv.writer(online_presence_file, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
 
 message_count = 0
 presence_count = 0
 
-for logfile_path in [f.path for f in os.scandir(tg_cli_logs_folder) if f.is_file() and f.name.startswith('complete_logs_')]:
+for logfile_path in sorted([f.path for f in os.scandir(tg_cli_logs_folder) if f.is_file() and f.name.startswith('complete_logs_')]):
     print(logfile_path)
     with open(logfile_path, 'r', encoding='utf-8') as logfile:
         for linenum, line in enumerate(logfile):
             line_without_ansi = line[line.find('{"'): -1]
             if len(line_without_ansi) < 10:
                 continue
-            obj = json.loads(line_without_ansi.encode('utf8'))
+            obj = {}
+            try:
+                obj = json.loads(line_without_ansi.encode('utf8'))
+            except json.decoder.JSONDecodeError as e:
+                print('PARSING ERROR. Offending line:')                
+                print(line_without_ansi)
             if 'text' in obj and obj.get("event","?") == 'message':
-                message_writer.writerow([str(obj['from']['peer_id']),
-                    obj['from']['print_name'],
-                    str(obj['to']['peer_id']),
-                    obj['to']['print_name'],
-                    str(datetime.datetime.utcfromtimestamp(int(obj['date'])).isoformat()),
-                    obj['text'].replace('\n', ' ')])
+                try:
+                    message_writer.writerow([str(obj['from']['peer_id']),
+                        obj['from']['print_name'],
+                        str(obj['to']['peer_id']),
+                        obj['to']['print_name'],
+                        str(datetime.datetime.utcfromtimestamp(int(obj['date'])).isoformat()),
+                        obj['text'].replace('\n', ' ')])
+                except Exception as e:
+                    print(obj)
+                    exit(1)
                 message_count += 1
                 continue
 
